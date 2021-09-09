73afcab0c998556fdde216c0563990563ef11b6c
Haidar <haidarisso98@gmail.com>
2021-09-09 10:09:26 +0200

    Wrong Passwort Popup

---
+++ src/sample/controller/PasswortErrorController.java
@@ -0,0 +1,12 @@
+package sample.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class PasswortErrorController {
+    @FXML
+    private AnchorPane paneError;
+    @FXML
+    private Button btnErrorOk;
+}
