4c6b4b0ade030fc805a30a1c2f65dbfd34de262b
IsabMueller <ki.asja.mueller@htwsaar.de>
2021-09-16 22:24:14 +0200

    register funzt wie login bites werden übertragen, error wird ausgeworen in post1.txt

--- src/sample/controller/RegisterController.java
+++ src/sample/controller/RegisterController.java
@@ -8,14 +8,23 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.AutoRetryHttpClient;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,8 +33,8 @@ public class RegisterController {
 
 
 
-    @FXML
-    private TextField tfBirthday;
+    //@FXML
+   // private TextField tfBirthday;
     @FXML
     private TextField tfRegMailwdh;
     @FXML
@@ -68,9 +77,9 @@ public class RegisterController {
         return tfRegVorname;
     }
 
-    public TextField getTfGeburtstag() {
-        return tfBirthday;
-    }
+    //public TextField getTfGeburtstag() {
+      //  return tfBirthday;
+   // }
 
     public TextField getTfRegNachname() {
         return tfRegNachname;
@@ -109,55 +118,70 @@ public class RegisterController {
 
 
 
-    public void submitBtnReg(ActionEvent event) {
+    public void submitBtnReg(ActionEvent event) throws UnsupportedEncodingException {
 
             firstName = tfRegVorname.getText();
             lastName = tfRegNachname.getText();
             username = tfRegUsername.getText();
-            birthday = tfBirthday.getText();
+           // birthday = tfBirthday.getText();
             mail = tfRegMail.getText();
             mailRepeat = tfRegMailwdh.getText();
             password = tfRegPw.getText();
             passwordRepeat = tfRegPwConfirm.getText();
             id = 0;     // id aufzählung? wie?
 
-        if (firstName.isEmpty() || lastName.isEmpty() || username.isEmpty() || mail.isEmpty() || mailRepeat.isEmpty() || password.isEmpty() || password.isEmpty()){
+        if (firstName.isEmpty() || lastName.isEmpty() || username.isEmpty() || mail.isEmpty() || mailRepeat.isEmpty() || password.isEmpty() || passwordRepeat.isEmpty()||(!(mail.equals(mailRepeat))) && (!(password.equals(passwordRepeat)))&&(!(password.equals(passwordRepeat)))&&(!(passwordRepeat.equals(password)))){
             System.out.println("Felder ausfüllen");
         }
-        else if (mail.equals(mailRepeat) && password.equals(passwordRepeat)) {
+        else {
 
             System.out.println(firstName);
             System.out.println(lastName);
             System.out.println(username);
             System.out.println(mail);
             System.out.println(mailRepeat);
-            System.out.println(birthday);
+            // System.out.println(birthday);
             System.out.println(password);
             System.out.println(passwordRepeat);
 
 
-        }
-
-        else if (mail.equals(mailRepeat) && password.equals(passwordRepeat)||password.equals(passwordRepeat)&&passwordRepeat.equals(password)) {
-            CloseableHttpClient httpClient = HttpClients.createDefault(); {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            {
                 HttpPost httpPost = new HttpPost("https://www.sharing-time.rest/auth/register");
                 List<NameValuePair> nvps = new ArrayList<>();
                 nvps.add(new BasicNameValuePair("firstname", firstName));
                 nvps.add(new BasicNameValuePair("lastname", lastName));
                 nvps.add(new BasicNameValuePair("username", username));
                 nvps.add(new BasicNameValuePair("mail", mail));
-                nvps.add(new BasicNameValuePair("birthday", birthday));
+                // nvps.add(new BasicNameValuePair("birthday", birthday));
                 nvps.add(new BasicNameValuePair("password", password));
+                httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+                try {
+                    httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
 
+
+                try (CloseableHttpResponse response2 = httpclient.execute(httpPost)) {
+                    // System.out.println(response2.getCode() + " " + response2.getReasonPhrase());
+                    HttpEntity entity2 = response2.getEntity();
+                    // do something useful with the response body
+                    // and ensure it is fully consumed
+                    try (OutputStream outputStream = Files.newOutputStream(Paths.get("post1.txt"))) {
+                        long length = entity2.getContent().transferTo(outputStream);
+                        System.out.println("Bytes uebertragen: " + length);
+
+                    }
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+
+
         }
 
-
-        else {
-            System.out.println("Fehler!");
-            //fensterpopup
         }
     }
-
-
-}
